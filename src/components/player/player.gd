731999fab6a5cea1d1e9b8f022ce85cf4be5626d
blackbrokkoli <kastomat@gmail.com>
2024-08-15 22:20:48 +0300

    add basic walking animation

--- src/components/player/player.gd
+++ src/components/player/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var player_sprite: AnimatedSprite2D = $PlayerSprite
 
 
 func _physics_process(delta: float) -> void:
@@ -13,12 +14,6 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -26,3 +21,17 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	update_animation(direction)
+	update_facing_direction(direction)
+	
+func update_animation(direction):
+	if direction != 0:
+		player_sprite.play("walk")
+	else:
+		player_sprite.play("idle")
+
+func update_facing_direction(direction):
+	if direction > 0:
+		player_sprite.flip_h = false
+	elif direction < 0:
+		player_sprite.flip_h = true
