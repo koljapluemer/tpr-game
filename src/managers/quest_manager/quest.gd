1516f7c2aab6806d4ea0f7cc9f5ef86f9748812b
blackbrokkoli <kastomat@gmail.com>
2024-08-16 18:50:01 +0300

    add basic statechart to quest

--- src/managers/quest_manager/quest.gd
+++ src/managers/quest_manager/quest.gd
@@ -4,17 +4,9 @@ class_name Quest extends Node2D
 @export var quest_description: String
 @export var start_dialog: Dialog
 @export var end_dialog: Dialog
+@export var is_available = false
 
+@onready var state_chart: StateChart = %StateChart
 
-enum QuestStatus {
-	unavailable,
-	available,
-	started, 
-	reached_goal,
-	finished
-}
-
-@export var quest_status: QuestStatus = QuestStatus.available
-
-func finish():
-	pass
+func _ready():
+	state_chart.set_expression_property("is_available", is_available)
