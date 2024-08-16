37914dc14d56e0d4baf2de691d7a41ce2e393676
blackbrokkoli <kastomat@gmail.com>
2024-08-16 19:10:46 +0300

    add simple appear event

--- src/managers/event_manager/events/event_appear_when_object_in_area.gd
+++ src/managers/event_manager/events/event_appear_when_object_in_area.gd
@@ -1,11 +1,7 @@
-class_name EventAppearWhenInArea extends Event
+class_name EventAppear extends Event
 
 @export var appearing_node: Node2D
-@export var target_area: Area2D
 
 func run():
-	target_area.area_entered.connect(_on_area_entered)
-
-func _on_area_entered():
 	appearing_node.show()
 	finish()
