bf51b8af281afa5fe428ad6f747443d1534b1c04
blackbrokkoli <kastomat@gmail.com>
2024-08-20 18:21:51 +0300

    remove state charts, reorganize

--- src/managers/quest_manager/quest.gd
+++ src/managers/quest_manager/quest.gd
@@ -1,16 +1,10 @@
 class_name Quest extends Node2D
 
-@export_group("Quest Settings")
-@export var quest_description: String
-@export var start_dialog: Dialog
-@export var end_dialog: Dialog
-@export var is_available = false
 
 
-@onready var state_chart: StateChart = %StateChart
 
 func _ready():
-	state_chart.set_expression_property("is_available", is_available)
+	pass
 
 func finish():
-	state_chart.send_event("set_finished")
+	pass
