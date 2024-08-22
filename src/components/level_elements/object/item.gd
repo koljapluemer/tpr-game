73daad7e2ebc8a37d6dcab10af1a03a7134df242
blackbrokkoli <kastomat@gmail.com>
2024-08-22 14:53:53 +0300

    start scene with interactable item

--- src/components/level_elements/object/item.gd
+++ src/components/level_elements/object/item.gd
@@ -5,6 +5,9 @@ class_name Item extends Resource
 @export var resize_factor:float = 1
 @export var offset = Vector2(0,0)
 @export var collision_radius = 100
-
+@export var interaction_radius = 300
+@export_category("German")
 @export var dative_form: String
 @export var word: String
+@export var interact_prompt: String
+@export var interact_demo: String
---
+++ src/components/level_elements/object/map_object_interactable.gd
@@ -0,0 +1,28 @@
+@tool
+class_name MapObjectInteractable extends MapObject
+
+
+@onready var player = get_tree().get_first_node_in_group("player")
+var player_within_interaction_range = false
+@onready var interaction_shape: Area2D = %InteractionShape2D
+@onready var interaction_collision_shape_2d: CollisionShape2D = %InteractionCollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if item:
+		interaction_collision_shape_2d.shape.radius = item.interaction_radius
+
+
+func _on_interaction_shape_2d_body_entered(body: Node2D) -> void:
+	if body == player:
+		player_within_interaction_range = true
+
+
+func _on_interaction_shape_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		player_within_interaction_range = false
+		
+# watch out: this uses collision shape, not interaction shape
+func _on_area_2d_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		print(name, ": I got clicked")
