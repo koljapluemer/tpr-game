e98ce91e789426efd8fabdf2eef3ab5e2c4585ee
blackbrokkoli <kastomat@gmail.com>
2024-08-23 20:12:30 +0300

    add very basic main menu

---
+++ src/levels/menus/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var first_level: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_packed(first_level)
