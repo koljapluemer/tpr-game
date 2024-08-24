72e9a9260cbf92975568c2bd7afcd423ec4b1d9f
blackbrokkoli <kastomat@gmail.com>
2024-08-24 18:16:04 +0300

    add basic level manager

--- src/levels/menus/main_menu.gd
+++ src/levels/menus/main_menu.gd
@@ -4,18 +4,9 @@ extends Control
 
 @onready var lang_select: OptionButton = %LangSelect
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_packed(first_level)
+	LevelManager.load_next_level()
 
 
 func _on_lang_select_item_selected(index: int) -> void:
