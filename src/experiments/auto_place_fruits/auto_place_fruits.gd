dddb3a597c1175bbe5b53a15d0002c291ed9b7b7
blackbrokkoli <kastomat@gmail.com>
2024-09-15 13:14:00 +0200

    mock fruit spawner

---
+++ src/experiments/auto_place_fruits/auto_place_fruits.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const APPLE = preload("res://src/components/level_elements/object/apple.tscn")
+const COCONUT = preload("res://src/components/level_elements/object/coconut.tscn")
+const PEAR = preload("res://src/components/level_elements/object/pear.tscn")
+const MELON = preload("res://src/components/level_elements/object/melon.tscn")
+
+const objects = [APPLE, COCONUT, PEAR, MELON]
+@onready var spawn_points: Node2D = %SpawnPoints
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for spawn_point in spawn_points.get_children():
+		var obj_to_spawn = objects.pick_random()
+		var obj = obj_to_spawn.instantiate()
+		add_child(obj)
+		obj.position = spawn_point.position
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
