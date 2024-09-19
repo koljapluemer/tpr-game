c1a70553fd5032b35bec2e464d053a8e04b6845c
blackbrokkoli <kastomat@gmail.com>
2024-09-19 12:18:34 +0200

    add bananas

--- src/level_stocking_shelves/level.gd
+++ src/level_stocking_shelves/level.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var apple_spawn_area_object = %AppleSpawnArea
 const APPLE = preload("res://src/level_stocking_shelves/apple.tscn")
+const BANANA = preload("res://src/level_stocking_shelves/banana.tscn")
+
+const FRUIT = [APPLE, BANANA]
 
 var apple_spawn_area
 var apple_spawn_origin
@@ -14,10 +17,11 @@ const SPAWN_DELAY = 0.02
 func _ready() -> void:
 	apple_spawn_area = apple_spawn_area_object.shape.extents
 	apple_spawn_origin = apple_spawn_area_object.global_position -  apple_spawn_area
-	spawn_apples()
+	spawn_objects()
 
-func spawn_apples():
-	var inst = APPLE.instantiate()
+func spawn_objects():
+	var obj_to_inst = FRUIT.pick_random()
+	var inst = obj_to_inst.instantiate()
 	var x = randi_range(apple_spawn_origin.x, apple_spawn_area.x)
 	var y = randi_range(apple_spawn_origin.y, apple_spawn_area.y)
 	inst.global_position.x = x
@@ -26,7 +30,7 @@ func spawn_apples():
 	apples.append(inst)
 	
 	if len(apples) < 20:
-		get_tree().create_timer(SPAWN_DELAY).connect("timeout", spawn_apples)
+		get_tree().create_timer(SPAWN_DELAY).connect("timeout", spawn_objects)
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
