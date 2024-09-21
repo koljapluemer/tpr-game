510b625187958c836f2684f4bf77ce332c88be70
blackbrokkoli <kastomat@gmail.com>
2024-09-21 08:21:36 +0200

    make objects takeable again

--- alchemy/001_test/components/takeable_component.gd
+++ alchemy/001_test/components/takeable_component.gd
@@ -12,5 +12,5 @@ func _ready() -> void:
 
 func _on_takeable_area_input(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
-		object_taken.emit(self)
-		queue_free()
+		print("click")
+		object_taken.emit()
