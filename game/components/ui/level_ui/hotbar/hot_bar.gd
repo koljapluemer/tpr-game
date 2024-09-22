cab8cfd54eafaa514bda4856796940a985e80f8b
blackbrokkoli <kastomat@gmail.com>
2024-09-22 19:52:24 +0200

    repair project to previous game state

--- game/components/ui/level_ui/hotbar/hot_bar.gd
+++ game/components/ui/level_ui/hotbar/hot_bar.gd
@@ -8,6 +8,6 @@ func set_buttons(modes:Dictionary):
 			var btn = INTERACTION_BUTTON.instantiate()
 			# TODO: this is rather dirty but I don't have better ideas either
 			# wouldn't know where to set this, except maybe global config?
-			btn.icon = load("res://alchemy/001_test/components/png/" + mode + ".png")
+			btn.icon = load("res://game/components/ui/level_ui/hotbar/interaction_buttons/assets/" + mode + ".png")
 			btn.mode = mode
 			add_child(btn)
