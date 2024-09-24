3b6f49e5c3255ea000a83349ea14ffc0bc065aa0
blackbrokkoli <kastomat@gmail.com>
2024-09-24 18:14:33 +0200

    actually make the quest gen dependent on words saved in an object

---
+++ game/components/quest_manager/quest.gd
@@ -0,0 +1,4 @@
+class_Name Quest extends Resource
+
+@export var key:Word
+@export var interaction:Interaction
--- game/components/quest_manager/quest_manager.gd
+++ game/components/quest_manager/quest_manager.gd
@@ -1,6 +1,13 @@
 class_name QuestManager extends Node
 
+@export var quest_ui:Container
+
+const LEVEL_UI_QUEST = preload("res://game/components/ui/level_ui/quests/level_ui_quest.tscn")
+
+var possible_quests
+
 func generate_possible_quests(objects:Array[ScrapbookObject]):
 	for obj:ScrapbookObject in objects:
-		for mode in obj.get_modes():
-			print("Possible Quest: ", obj.name, "—", mode.key)
+		for word in obj.words:
+			for mode in obj.get_modes():
+				print("Possible Quest: ", word.key, "—", mode.key)
