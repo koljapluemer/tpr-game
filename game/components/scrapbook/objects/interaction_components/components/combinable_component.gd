0776e97a23de07fcf543689197c1c9c443bec2f6
blackbrokkoli <kastomat@gmail.com>
2024-09-26 14:23:24 +0200

    delete outdated interaction components

--- game/components/scrapbook/objects/interaction_components/components/combinable_component.gd
+++
@@ -1,9 +0,0 @@
-## An [InteractionComponent] allowing an object to be cut
-class_name CombinableComponent extends InteractionComponent
-
-func _ready() -> void:
-	pass
-
-func _on_body_entered(body):
-	pass
-	
--- game/components/scrapbook/objects/interaction_components/components/takeable_component.gd
+++
@@ -1,8 +0,0 @@
-## An [InteractionComponent] allowing an object to be taken
-## Currently not functional in any way.
-class_name TakeableComponent extends InteractionComponent
-
-func _react_to_input() -> bool:
-	owner.queue_free()
-	return true
-	
