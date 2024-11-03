c21967e9e2db25733d8ead03d03f44eceb8feca9
blackbrokkoli <kastomat@gmail.com>
2024-11-03 11:26:02 +0100

    move menu buttons out of the way

--- game/ui/level_ui/level_ui.gd
+++ game/ui/level_ui/level_ui.gd
@@ -1,6 +1,8 @@
 ## The manager for the user interface visible while playing a level
 extends CanvasLayer
 
+@onready var popup: Popup = %Popup
+
 
 func _on_restart_button_pressed() -> void:
 	SceneManager.reload_level()
@@ -11,3 +13,7 @@ func _on_end_button_pressed() -> void:
 func _on_main_menu_button_pressed() -> void:
 	SceneManager.load_main_menu()
 	
+
+
+func _on_menu_button_pressed() -> void:
+	popup.visible = true
